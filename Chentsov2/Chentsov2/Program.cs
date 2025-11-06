f1133c26b5e0e74f3fdd18116c889eaf65ce96fa
shtrudellll <egor.chensov@gmail.com>
2025-11-06 18:40:45 +0100

    +binary search
    +binary search

--- Chentsov2/Chentsov2/Program.cs
+++ Chentsov2/Chentsov2/Program.cs
@@ -1,20 +1,35 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        int[] pole = {1, 2, 4, 8, 15};
-        Console.WriteLine(HledejMaximum(pole));
+        int[] pole = { 1, 2, 4, 8, 15 };
 
-        // EXISTUJE  Array.Sort(Pole); !!!!!!!!!111111!!!!!!!!!!!!!!1 
-        Console.WriteLine(SerazeniCisel(pole));
+        Console.WriteLine("Maximum: " + HledejMaximum(pole));
+
+        Console.WriteLine("Seřazeno (sestupně): " + SerazeniCisel(pole));
+
+        //Test 4...
+        int hledaneCislo = 8;
+        int index = BinarySearch(pole, hledaneCislo);
+        //yay
+
+
+        if (index != -1)
+        {
+            Console.WriteLine($"Číslo {hledaneCislo} ma index {index}");
+        }
+        else
+        {
+            Console.WriteLine($"Číslo {hledaneCislo} neni");
+        }
     }
 
-    static int HledejMaximum(int[]  Pole)
+    static int HledejMaximum(int[] Pole)
     {
-
-        int max = Pole[0]; 
-
+        int max = Pole[0];
         foreach (int cislo in Pole)
         {
             if (cislo > max)
@@ -22,9 +37,9 @@
                 max = cislo;
             }
         }
-
         return max;
     }
+
     static string SerazeniCisel(int[] Pole)
     {
         for (int i = 0; i < Pole.Length - 1; i++)
@@ -44,5 +59,29 @@
 
         return string.Join(" ", Pole);
     }
+    static int BinarySearch(int[] Pole, int hledaneCislo)
+    {
+        int left = 0;
+        int right = Pole.Length - 1;
 
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (Pole[mid] == hledaneCislo)
+            {
+                return mid;
+            }
+
+            if (Pole[mid] < hledaneCislo)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
 }
