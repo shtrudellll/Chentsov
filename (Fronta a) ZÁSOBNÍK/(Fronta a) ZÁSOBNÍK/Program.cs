c74f9a7ce98ed7882e99afa5c17ea18f6ee1ad31
shtrudellll <egor.chensov@gmail.com>
2025-12-30 14:53:22 +0100

    yaay

---
+++ (Fronta a) ZÁSOBNÍK/(Fronta a) ZÁSOBNÍK/Program.cs
@@ -0,0 +1,65 @@
+﻿   namespace _Fronta_a__ZÁSOBNÍK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string seznam = ("{[()()](");
+            Stack<string> s = new Stack<string>();
+
+            string[] ot = ["{", "[", "("];
+            string[] zav = ["}", "]", ")"];
+
+            for (int i = 0; i < seznam.Length; i++)
+            {
+                if (s.Count == 0)
+                {
+                    s.Push(seznam[i].ToString());
+
+                }
+                else
+                {
+                    if (seznam[i].ToString() == ot[0] || seznam[i].ToString() == ot[1] || seznam[i].ToString() == ot[2])
+                    {
+                        s.Push(seznam[i].ToString());
+                    }
+                else if (seznam[i].ToString() == zav[0] || seznam[i].ToString() == zav[1] || seznam[i].ToString() == zav[2])
+                    {
+                        string a = s.Peek();
+                        if (seznam[i].ToString() == zav[0] && a == ot[0])
+                        {
+                            s.Pop();
+
+                        }
+                        else if (seznam[i].ToString() == zav[1] && a == ot[1])
+                        {
+                            s.Pop();
+
+                        }
+                        else if (seznam[i].ToString() == zav[2] && a == ot[2])
+                        {
+                            s.Pop();
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("nuh uh");
+                            return;
+                        }
+                    }
+                }
+            }
+            if (s.Count == 0)
+            {
+                Console.WriteLine("asi to jde");
+            }
+            else
+            {
+                Console.WriteLine("nuh uh");
+            }
+            
+
+
+        }
+    }
+}
---
+++ aaaaa/aaaaa/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Zadej číslo: ");
+        long n = long.Parse(Console.ReadLine());
+
+        Console.Write("Prvočíselný rozklad: ");
+
+        for (long i = 2; i * i <= n; i++)
+        {
+            while (n % i == 0)
+            {
+                Console.Write(i + " ");
+                n /= i;
+            }
+        }
+
+        if (n > 1)
+            Console.Write(n); // zbytek je prvočíslo
+
+        Console.WriteLine();
+    }
+}
