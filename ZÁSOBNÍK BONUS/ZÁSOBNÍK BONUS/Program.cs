53878cee2fc5dc11266651c44c668286cd309deb
shtrudellll <egor.chensov@gmail.com>
2026-01-01 17:25:04 +0100

    bonus

---
+++ ZÁSOBNÍK BONUS/ZÁSOBNÍK BONUS/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<int> rozklad = new List<int>();
+        Rozklad(n, 1, rozklad);
+    }
+
+    static void Rozklad(int zbytek, int minimum, List<int> aktualni)
+    {
+
+        if (zbytek == 0)
+        {
+            Console.WriteLine(string.Join("+", aktualni));
+            return;
+        }
+
+        for (int i = minimum; i <= zbytek; i++)
+        {
+            aktualni.Add(i);
+            Rozklad(zbytek - i, i, aktualni);
+            aktualni.RemoveAt(aktualni.Count - 1); 
+        }
+    }
+}
