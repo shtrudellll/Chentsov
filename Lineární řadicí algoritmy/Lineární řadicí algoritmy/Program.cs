8cf174e759aced9824ea686ddcb88aff29479e27
shtrudel <egor.chensov@gmail.com>
2026-01-19 16:42:13 +0100

    linearni radici alg

---
+++ Lineární řadicí algoritmy/Lineární řadicí algoritmy/Program.cs
@@ -0,0 +1,102 @@
+﻿namespace PololetniUloha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // (20b) 1. Seřaďte známky ze souboru znamky.txt od 1 do 5 algoritmem s lineární časovou složitostí vzhledem k počtu známek.
+            // Vypište je na řádek a pak vypište i četnosti jednotlivých známek.
+            using (StreamReader sr = new StreamReader(@"znamky.txt")) // otevření souboru pro čtení
+            {
+
+                int[] massive = [0, 0, 0, 0, 0];
+                while (!sr.EndOfStream) // dokud jsme nedošli na konec souboru
+                {
+                    int znamka = Convert.ToInt16(sr.ReadLine()); // čteme známky po řádcích a převádíme je na číslo
+
+                    massive[znamka - 1] += 1;
+
+                }
+
+                //int n = 1;
+                for (int i = 1; i < 6; i++)
+                {
+                    for (int j = 0; j < massive[i - 1]; j++)
+                    {
+                        Console.Write(i + " ");
+                        //Console.Write(n+" ");
+                        //n+=1;
+                    }
+
+                    Console.WriteLine(Convert.ToString("pocet " + massive[i - 1]));
+                }
+
+
+
+
+            }
+            // => to, co jste pravděpodobně stvořili se nazývá Counting Sort cool =)
+
+
+
+            // (40b) 2. Ze souboru znamky_prezdivky.csv vytvořte objekty typu Student se správně přiřazenou známkou a přezdívkou.
+            // Seřaďte je podle známek (stabilně = dodržte pořadí v souboru) a vypište seřazené dvojice (znamka: přezdívka) - na každý řádek jednu.
+            using (StreamReader sr = new StreamReader("znamky_prezdivky.csv"))
+            {
+                // 5 přihrádek (index 0 = známka 1, ..., index 4 = známka 5)
+                List<Student>[] typky = new List<Student>[5];
+
+                for (int i = 0; i < 5; i++)
+                {
+                    typky[i] = new List<Student>();
+                }
+
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(';');
+
+                    int znamka = Convert.ToInt32(line[1]);
+                    string prezdivka = line[0];
+
+                    Student s = new Student(prezdivka, znamka);
+
+
+                    typky[znamka - 1].Add(s);                                                     //-1!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                }
+
+
+                for (int i = 0; i < 5; i++)
+                {
+                    foreach (Student s in typky[i])
+                    {
+                        Console.WriteLine($"{s.Prezdivka} ma {s.Znamka}");
+                    }
+                }
+            }
+            // => to, co jste pravděpodobně stvořili se nazývá Bucket Sort (přihrádkové řazení)
+
+
+
+
+            // (10b) 3. Určete časovou a prostorovou složitost algoritmu z 2. úkolu
+
+
+
+
+
+
+        }
+    }
+
+    class Student
+    {
+        public string Prezdivka { get; } // tím, že je zde pouze get říkáme, že tato vlastnost třídy Student jde mimo třídu pouze číst, nikoli upravovat
+        public int Znamka { get; }
+        public Student(string prezdivka, int znamka) // konstruktor třídy
+        {
+            // použitím samotného { get; } také říkáme, že tyto vlastnosti jdou nastavit nejpozději v konstruktoru - tedy v této metodě
+            Prezdivka = prezdivka;
+            Znamka = znamka;
+        }
+    }
+}
