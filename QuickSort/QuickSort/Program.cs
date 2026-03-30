50bb0823a50efcd6a96f0d1c4df14a504286bdf2
shtrudel <egor.chensov@gmail.com>
2026-03-30 17:23:59 +0200

    QuickSort
    QuickSort

---
+++ QuickSort/QuickSort/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Net.Quic;
+
+namespace QuickSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 6, 3, 5, 2, 1, 8, 9, 3, 4, 9, 12, 29, 1234 };
+            numbers=QS(numbers);
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine(numbers[i]);          
+            }
+        }
+        public static List<int> QS(List<int> numbers)
+        {
+            if (numbers.Count <= 1)
+            {
+                List<int> ready = numbers;
+                return numbers;
+            }
+            else
+            {
+                int pivo = numbers[numbers.Count / 2];
+                List<int> L = new List<int>();
+                List<int> P = new List<int>();
+                List<int> S = new List<int>();
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    if (numbers[i] == pivo)
+                    {
+                        S.Add(numbers[i]);
+                    }
+                    if (numbers[i] > pivo)
+                    {
+                        P.Add(numbers[i]);
+                    }
+                    if (numbers[i] < pivo)
+                    {
+                        L.Add(numbers[i]);
+                    }
+                }
+
+                List<int> result = new List<int>();
+
+                result.AddRange(QS(L));
+                result.AddRange(S);
+                result.AddRange(QS(P));
+
+                return result;
+
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//kam mame vubec psat bonusy..?
+
+//
+
+//4. ve vstupech kde medián neni hodnota nejbližší průměru jako [1, 2, 3, 4, 1000]
---
+++ Topologické uspořádání/Topologické uspořádání/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _15_Abecedni_poradi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte vztahy (např. c<b a<c):");
+            string[] vztahy = Console.ReadLine().Split();
+
+            // 1. Zjistíme všechny znaky
+            List<char> znakyAbecedy = new List<char>();
+            foreach (string vztah in vztahy)
+            {
+                if (!znakyAbecedy.Contains(vztah[0]))
+                    znakyAbecedy.Add(vztah[0]);
+                if (!znakyAbecedy.Contains(vztah[2]))
+                    znakyAbecedy.Add(vztah[2]);
+            }
+
+            int pocetVrcholu = znakyAbecedy.Count;
+
+            // 2. Vytvoříme graf (matice sousednosti)
+            int[,] graf = new int[pocetVrcholu, pocetVrcholu];
+            foreach (string vztah in vztahy)
+            {
+                int indexZ = znakyAbecedy.IndexOf(vztah[0]);
+                int indexDo = znakyAbecedy.IndexOf(vztah[2]);
+                graf[indexZ, indexDo] = 1;
+            }
+
+            // 3. Spočítáme vstupní stupně (in-degree)
+            int[] stupneVrcholu = new int[pocetVrcholu];
+            for (int i = 0; i < pocetVrcholu; i++) // pro každý sloupec
+            {
+                int suma = 0;
+                for (int j = 0; j < pocetVrcholu; j++)
+                    suma += graf[j, i];
+                stupneVrcholu[i] = suma;
+            }
+
+            // 4. Najdeme všechny zdroje (in-degree = 0) a vložíme do "fronty" (seznam pro abecední výběr)
+            List<int> fronta = new List<int>();
+            for (int i = 0; i < pocetVrcholu; i++)
+                if (stupneVrcholu[i] == 0)
+                    fronta.Add(i);
+
+            List<char> topologickeUsporadani = new List<char>();
+
+            while (fronta.Count > 0)
+            {
+                // Vyber abecedně nejmenší znak
+                fronta.Sort((a, b) => znakyAbecedy[a].CompareTo(znakyAbecedy[b]));
+                int vrchol = fronta[0];
+                fronta.RemoveAt(0);
+
+                topologickeUsporadani.Add(znakyAbecedy[vrchol]);
+
+                // Snížíme in-degree sousedů
+                for (int i = 0; i < pocetVrcholu; i++)
+                {
+                    if (graf[vrchol, i] == 1)
+                    {
+                        stupneVrcholu[i]--;
+                        if (stupneVrcholu[i] == 0)
+                            fronta.Add(i);
+                    }
+                }
+            }
+
+            // 5. Kontrola cyklu
+            if (topologickeUsporadani.Count != pocetVrcholu)
+            {
+                Console.WriteLine("Graf obsahuje cyklus, topologické uspořádání nelze vytvořit.");
+            }
+            else
+            {
+                Console.WriteLine("Topologické uspořádání: " + string.Join(" < ", topologickeUsporadani));
+            }
+        }
+    }
+}
